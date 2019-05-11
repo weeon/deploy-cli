af624a14539b41c1744ec5a7e0b4cedae826cf5d
orvice <orvice@gmail.com>
2019-05-11 20:49:25 +0800

    add action flag

--- cmd/deploy-cli/main.go
+++ cmd/deploy-cli/main.go
@@ -1,12 +1,32 @@
 package main
 
-import "github.com/weeon/deploy-cli/internal/biz"
+import (
+	"flag"
+	"fmt"
+	"os"
 
-func main(){
+	"github.com/weeon/deploy-cli/internal/biz"
+)
+
+var (
+	action string
+)
+
+func main() {
 	var err error
 	err = biz.Init()
-	if err != nil{
-
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
 	}
-	biz.Deploy()
+	flag.Parse()
+	flag.StringVar(&action, "action", "", "action")
+
+	switch action {
+	case "deploy":
+		biz.Deploy()
+	default:
+		fmt.Println("no action input")
+	}
+
 }
